415cf94c6cd0915baf3853a8e9c2347531252a2e
DaviAraki <davi.araki@gmail.com>
2020-02-06 17:05:07 -0300

    Update App.js
    Adicionadas varias funcionalidades para single player

--- footrealms/src/App.js
+++ footrealms/src/App.js
@@ -12,6 +12,7 @@ const FootRealms = {
                     deck:[],
                     admZone:[],
                     playZone:[],
+                    discardZone:[],
                     money:0,
                     score:0,
                     points:0
@@ -20,7 +21,8 @@ const FootRealms = {
             offer:{
                 offerZone:[],
                 deck:[],
-                turn:0
+                turn:0,
+                desafio:0
             }
         }
     ),
@@ -32,40 +34,36 @@ const FootRealms = {
    
     endIf:(G, ctx)=>{
         if(G.offer.turn===8){
-            let maxPoints = 0;
-            let leader = G.players[0];
-            for(var i=0;i < G.players.length;i++){
-                if(G.players.points>maxPoints){
-                    maxPoints = G.players.points
-                    leader = G.players[i];
-                }
-                if(G.players.points===maxPoints){
-                    leader = drawGame;
-                }
-            }
-            if(leader === drawGame){
-                return {draw : true}
-            }
-            return{winner : leader};
+            return G.players[0].points;
         }
     },
    
     phases:{
         inicio:{
             moves:{selectCard,pass},
+            onBegin: {
+                drawHand(G,ctx);,
+                setDesafio(G,ctx)    
+            },
             next:'disputa',
             start: true,
         },
         disputa:{
+            moves:{pass},
+            onBegin:{
+                setChuteira(G,ctx);,
+                defineWinner(G,ctx);,
+            },
             next:'administracao'
                 
         },
         administracao:{
-            moves:{playCard,pass,buyCard},
+            moves:{playCard,pass,buyCard,discardCard},
             next: 'classificacao',
 
         },
         classificacao:{
+            moves:{pass},
             onEnd: (G,ctx)=>{
                 for(i=0; i<G.players.length; i++){
                     while(G.players[i].hand.length<5){
@@ -88,6 +86,39 @@ export function draw(G, ctx){
       }
       G.players[ctx.currentPlayer].admZone.push(G.players[ctx.currentPlayer].deck.pop());
 }
+export function setChuteira(G,ctx) {
+    while(G.ctx.currentPlayer.hand.length>0){
+        let i = 0;
+        G.players[ctx.currentPlayer].score = G.players[ctx.currentPlayer].score + G.players[ctx.currentPlayer].hand[i].chuteira;
+    }    
+}
+export function setDesafio(G, ctx) {
+    if(G.offer.turn<2){
+        for(let i = 0; i>2; i++){
+            G.offer.desafio = G.offer.desafio +  G.offer.offerZone[i].chuteira
+        }
+    }else if(G.offer.turn<4){
+            for(let i = 0; i>3; i++){
+                G.offer.desafio = G.offer.desafio +  G.offer.offerZone[i].chuteira
+            }
+        } else if(G.offer.turn<6){
+            for(let i = 0; i>4; i++){
+                G.offer.desafio = G.offer.desafio +  G.offer.offerZone[i].chuteira
+            }
+        }else if(G.offer.turn<8){
+            for(let i = 0; i>5; i++){
+                G.offer.desafio = G.offer.desafio +  G.offer.offerZone[i].chuteira
+            }
+        }
+        
+}
+export function defineWinner(G,ctx) {
+    if(G.players[ctx.currentPlayer].chuteira > G.offer.desafio){
+        G.players.points = G.players.points + 3;
+    }
+    G.players.chuteira = 0;
+    G.offer.desafio = 0;
+}
 export function shuffle(G, ctx){
     G.players[ctx.currentPlayer].deck = ctx.random.Shuffle(G.players[ctx.currentPlayer].deck);
 }
@@ -106,6 +137,10 @@ export function selectCard(G,ctx,cardIndex) {
     G.players[ctx.currentPlayer].admZone.push(G.players[ctx.currentPlayer].hand[cardIndex]);
     G.players[ctx.currentPlayer].hand.splice(cardIndex,1);   
 }
+export function discardCard(G,ctx,cardIndex) {
+    G.players[ctx.currentPlayer].discardZone.push(G.players[ctx.currentPlayer].admZone[cardIndex]);
+    G.players[ctx.currentPlayer].admZone.splice(cardIndex,1);   
+}
 export function buyCard(G,ctx,cardIndex) {
     if(G.players[ctx.currentPlayer].money>=G.offer.offerZone[cardIndex].cost){
         var cost = G.offer.offerZone[cardIndex].cost;
@@ -113,6 +148,12 @@ export function buyCard(G,ctx,cardIndex) {
         G.players[ctx.currentPlayer].money= G.players[ctx.currentPlayer].money - cost;
         G.offer.offerZone.splice(cardIndex,1); 
     }    
+}
+export function drawHand(G,ctx) {
+    while(G.ctx.currentPlayer.hand.length<5){
+        drawHand(G,ctx);
+    }
+    
 }
 const App = Client({game: FootRealms});
 
