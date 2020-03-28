d3d783b1295e9659bab82c8967accbe9e7233b0e
Igor Knop <igorknop@gmail.com>
2020-03-28 00:49:58 -0300

    Add playerboard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GameCard from "./GameCard";
+import GamePlayerBoard from "./GamePlayerBoard";
 
 const styleBoard = {
     backgroundColor: "lightgreen",
@@ -8,6 +9,11 @@ const styleBoard = {
 }
 const styleOffer = {
     backgroundColor: "green",
+    border: "1px solid black",
+    color: "white",
+}
+const stylePlayers = {
+    backgroundColor: "blue",
     border: "1px solid black",
     color: "white",
 }
@@ -26,7 +32,9 @@ export default class GameBoard extends React.Component {
         let cards = this.props.G.offer.offerZone.map(card => 
             <GameCard card={card} />
         );
-
+        let players = this.props.G.players.map(player => 
+            <GamePlayerBoard player={player} />
+        );
         return (
             <div className="game-board" style={styleBoard}>
                 <div className="card-offer" style={styleOffer}>
@@ -35,6 +43,15 @@ export default class GameBoard extends React.Component {
                         {cards}
                     </div>
                 </div>
-            </div>)
+                <div className="card-players" style={styleOffer}>
+                    <div>Players:</div>
+                    <div className="card-offer-players" style={stylePlayers}>
+                        {players}
+                    </div>
+                </div>
+            
+            </div>
+            
+            )
     }
 }
