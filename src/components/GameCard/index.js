b273e5281049f8ef616a8830d42440aadce7155a
Igor Knop <igorknop@gmail.com>
2020-04-09 21:03:43 -0300

    Change card style

---
+++ src/components/GameCard/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+
+export default class GameCard extends React.Component {
+  render() {
+    return (
+      <div className="game-card">
+        <div className="name">{this.props.card.name}</div>
+        <div className="coin">
+          <span role="img" aria-label="Money">
+            💰
+          </span>
+          <div>{this.props.card.coin}</div>
+        </div>
+        <div className="boots">
+          <span role="img" aria-label="Skill">
+            ⚽
+          </span>
+          <div>{this.props.card.chuteira}</div>
+        </div>
+        <div className="cost">
+          <span role="img" aria-label="Money">
+            💰
+          </span>
+          <div>{this.props.card.cost}</div>
+        </div>
+        <div className="power">
+          <label>Power:</label>
+          {this.props.card.power}
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/GamePlayerBoard.js
+++ src/components/GamePlayerBoard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import GameCard from "./GameCard";
+import GameCard from "./GameCard/";
 
 const styleplayerBoard = {
     backgroundColor: "lightblue",
