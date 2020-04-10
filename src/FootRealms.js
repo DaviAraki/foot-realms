9446127d3bbbcb80b7bf4890f2022d433d1c40a5
Igor Knop <igorknop@gmail.com>
2020-04-10 05:30:09 -0300

    Create FootRealms class with static constants

---
+++ src/FootRealms.js
@@ -0,0 +1,8 @@
+export default class FootRealms {
+  static get TO_ADM() {
+    return 0;
+  }
+  static get TO_HAND() {
+    return 1;
+  }
+}
--- src/tests/draw.test.js
+++ src/tests/draw.test.js
@@ -1,25 +1,25 @@
 import commonFoward from "../components/Cards/commonFoward";
 import { draw } from "../App";
+import FootRealms from "../FootRealms";
 
-it ("When i draw, the top card of my deck goes to my hand", ()=>{
-    const f = new commonFoward();
-    const G = {
-        players:[
-            {
-               name: "Player A",
-               hand:[],
-               deck:[f],
-               admZone:[],
-               playZone:[],
-               money:1,
-               score:0,
-               points:0
-             }
-        ]
-    };
-    draw(G, {currentPlayer:"0"},1);
-    expect(G.players[0].deck.length).toEqual(0);
-    expect(G.players[0].hand.length).toEqual(1);
-    expect(G.players[0].hand[0]).toBe(f);
-
-})
+it("When i draw, the top card of my deck goes to my hand", () => {
+  const f = new commonFoward();
+  const G = {
+    players: [
+      {
+        name: "Player A",
+        hand: [],
+        deck: [f],
+        admZone: [],
+        playZone: [],
+        money: 1,
+        score: 0,
+        points: 0,
+      },
+    ],
+  };
+  draw(G, { currentPlayer: "0" }, FootRealms.TO_HAND);
+  expect(G.players[0].deck.length).toEqual(0);
+  expect(G.players[0].hand.length).toEqual(1);
+  expect(G.players[0].hand[0]).toBe(f);
+});
