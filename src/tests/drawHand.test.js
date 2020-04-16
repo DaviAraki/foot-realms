cf4a32014548f2cc0570d6859f2a65fda56203e6
DaviAraki <davi.araki@gmail.com>
2020-04-16 13:36:24 -0300

    Update drawHand.test.js

--- src/tests/drawHand.test.js
+++ src/tests/drawHand.test.js
@@ -20,3 +20,23 @@ it ("When the turn begins i draw until i have 5 cards", ()=>{
     expect(G.players[0].hand.length).toEqual(5);
     expect(G.players[0].deck.length).toEqual(0);
 });
+it("When the turn begins i draw until i have 5 cards, if my deck run out of cards the discardZone shall be shuffled", () => {
+    const G = {
+        players: [
+            {
+                name: "Player A",
+                hand: [],
+                deck: [new commonFoward(), new commonFoward(), new commonFoward()],
+                admZone: [],
+                discardZone: [new commonFoward(), new commonFoward()],
+                playZone: [],
+                money: 1,
+                score: 0,
+                points: 0
+            }
+        ]
+    };
+    drawHand(G, { currentPlayer: "0" }, 1);
+    expect(G.players[0].hand.length).toEqual(5);
+    expect(G.players[0].deck.length).toEqual(0);
+});
