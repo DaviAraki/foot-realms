b780069e59c9e1f064e17c3dab672a55ad1ffde7
DaviAraki <davi.araki@gmail.com>
2020-04-16 15:32:34 -0300

    Update phasePass.test.js

--- src/tests/phasePass.test.js
+++ src/tests/phasePass.test.js
@@ -43,3 +43,44 @@ it ("when i pass phase, another should begin", ()=>{
     pass(G,ctx);
     expect(ctx.phase).toBe("disputa");
 })
+it("when i pass the classification phase, inicio should begin", () => {
+    const G = {
+        offer: {
+            turn: 0
+        },
+        phases: {
+            inicio: {
+                moves: { selectCard, pass },
+                next: 'disputa',
+                start: true,
+            },
+            disputa: {
+                next: 'administracao'
+
+            },
+            administracao: {
+                moves: { playCard, pass, buyCard },
+                next: 'classificacao',
+
+            },
+            classificacao: {
+                onEnd: (G, ctx) => {
+                    for (i = 0; i < G.players.length; i++) {
+                        while (G.players[i].hand.length < 5) {
+                            draw(G, ctx);
+                        };
+                    };
+                    G.offer.turn++;
+                },
+                next: 'inicio',
+            }
+        },
+    };
+    let ctx = { phase: "classificacao", events: {} };
+    let endPhase = jest.fn(() => {
+        ctx.phase = "inicio";
+    });
+    ctx.events.endPhase = endPhase;
+    pass(G, ctx);
+    expect(ctx.phase).toBe("inicio");
+})
