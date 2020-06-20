c270327e65bda23cbbe0ca6cc36a29f325b02585
DaviAraki <davi.araki@gmail.com>
2020-06-20 18:57:00 -0300

    Game connecting to the server

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Client } from "boardgame.io/react";
 import {FootRealms} from './Game';
 import "./App.css";
 import GameBoard from "./components/GameBoard";
+import { SocketIO } from 'boardgame.io/multiplayer';
 
 
 const App = Client({
    game: FootRealms, 
    board: GameBoard, 
    numPlayers : 1,
+   multiplayer: SocketIO({ server: 'localhost:8000' }),
   });
 
 export default App;
