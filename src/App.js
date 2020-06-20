b91a2361bdf31d6f061ac2c04f143cad14130498
DaviAraki <davi.araki@gmail.com>
2020-06-20 19:28:18 -0300

    Client running

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Client } from "boardgame.io/react";
 import {FootRealms} from './Game';
 import "./App.css";
+import React from 'react';
 import GameBoard from "./components/GameBoard";
 import { SocketIO } from 'boardgame.io/multiplayer';
 
 
-const App = Client({
+const FootReamlsClient = Client({
    game: FootRealms, 
    board: GameBoard, 
-   numPlayers : 1,
    multiplayer: SocketIO({ server: 'localhost:8000' }),
   });
+
+const App = () =>(
+  <div>
+    <FootReamlsClient playerID="0" />
+</div>
+) 
 
 export default App;
