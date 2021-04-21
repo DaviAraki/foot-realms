97034bfb038e4cf8f332c0ce9c31945052f79cb4
Igor Knop <igorknop@gmail.com>
2021-04-21 17:22:24 -0300

    Fix score tab css

--- src/components/ScoreTrack/index.js
+++ src/components/ScoreTrack/index.js
@@ -1,18 +1,19 @@
 import React from "react";
-
+import "./styles.css";
 
 export default function ScoreTrack({ G, ctx, moves, events }) {
   const points = [];
   for (let i = 0; i < 22; i++) {
     points.push(
       <div className="score-square">
-        {i + "\n"}
-        <div className="teamPositions">
-          {G.players[0].points === i ? "P0 " : ""}
-          {G.offer.dummies.map((dummy) =>
-            dummy.points === i ? dummy.name + " " : ""
-          )}
-        </div>
+        <div className="score-value">{i}</div>
+        {G.players.map((player) =>
+          player.points === i ? <div>{player.name}</div> : ""
+        )}
+
+        {G.offer.dummies.map((dummy) =>
+          dummy.points === i ? <div>{dummy.name}</div> : ""
+        )}
       </div>
     );
   }
