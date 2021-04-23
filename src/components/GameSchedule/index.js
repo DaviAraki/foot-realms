66687442981632f3a14c0cb55c3fab49dea5c2ca
Davi Araki <davi.araki@gmail.com>
2021-04-23 12:23:55 -0300

    Update index.js

--- src/components/GameSchedule/index.js
+++ src/components/GameSchedule/index.js
@@ -5,15 +5,15 @@ export default function GameSchedule({ G, ctx, moves, events }) {
   if (!G) return <div></div>;
   //const schedule = G.schedule;
   const teams = [G.players[0], ...G.offer.dummies];
-  
+
   const schedule = [
-    [[0,1],[2,3],[4,5],[6,7]],
-    [[0,2],[1,3],[4,6],[5,7]],
-    [[0,3],[2,1],[4,7],[6,5]],
-    [[0,1],[2,3],[4,5],[6,7]],
-    [[0,1],[2,3],[4,5],[6,7]],
-    [[0,1],[2,3],[4,5],[6,7]],
-    [[0,1],[2,3],[4,5],[6,7]],
+    [[0, 1], [2, 3], [4, 5], [6, 7]],
+    [[0, 2], [1, 3], [4, 6], [5, 7]],
+    [[0, 3], [2, 1], [4, 7], [6, 5]],
+    [[0, 1], [2, 3], [4, 5], [6, 7]],
+    [[0, 1], [2, 3], [4, 5], [6, 7]],
+    [[0, 1], [2, 3], [4, 5], [6, 7]],
+    [[0, 1], [2, 3], [4, 5], [6, 7]],
 
   ];
 
@@ -21,7 +21,7 @@ export default function GameSchedule({ G, ctx, moves, events }) {
     const matches = round.map((match, m) => <li key={`match${r}${m}`}>{teams[match[0]].name} x {teams[match[1]].name}</li>);
     return (
       <li key={`round${r}`} className="round">
-        <h1>Round {r}</h1>
+        <h1>Round {r + 1}</h1>
         <ul>{matches}</ul>
       </li>
     );
