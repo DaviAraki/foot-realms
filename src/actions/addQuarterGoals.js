44885ffbff2fc8613a3d53fd37624be76d2b8482
Davi Araki <davi.araki@gmail.com>
2021-04-25 20:22:58 -0300

    Update addQuarterGoals.js

--- src/actions/addQuarterGoals.js
+++ src/actions/addQuarterGoals.js
@@ -7,17 +7,19 @@ export default function addQuarterGoals(G, ctx) {
         match.b.goals = (match.b.goals === '-') ? 0 : match.b.goals;
         if(ctx.turn % 2 === 0){
             if (teams[match.a.id].strength >= teams[match.b.id].strength) {
-                match.a.goals = match.a.goals + 1
                 teams[match.a.id].goals = teams[match.a.id].goals +1
-                teams[match.b.id].goalsAgainst = teams[match.b.id].goalsAgainst +1
+                teams[match.b.id].goalsAgainst = teams[match.b.id].goalsAgainst + 1
             }
         }
         else {
             if (teams[match.a.id].strength <= teams[match.b.id].strength) {
                 match.b.goals = match.b.goals + 1
                 teams[match.b.id].goals = teams[match.b.id].goals +1
+
                 teams[match.a.id].goalsAgainst = teams[match.a.id].goalsAgainst +1
         }
         };
+        match.a.goals = teams[match.a.id].goals
+        match.b.goals = teams[match.b.id].goals
     }
 }
